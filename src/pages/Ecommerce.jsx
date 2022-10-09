765292acadf2aaf747032ab480e5b42310f5dc23
Antonio1369 <mcuencad@uni.pe>
2022-10-09 12:54:29 -0500

    Agregando grafico

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -15,7 +15,7 @@ const Ecommerce = () => {
 
           <div className='flex justify-between items-center'>
             <div>
-              <p className='font-bold text-gray-400'>Earnings</p>
+              <p className='font-bold text-gray-400'>Parámetros del Rover</p>
               <p className='text-2xl'>$456532.13 </p>
             </div>
 
@@ -46,19 +46,75 @@ const Ecommerce = () => {
               >
               {item.icon}
               </button>
-
-              <p className='mt-3'>
-                <span className='text-lg font-semibold'>
-                  {item.amount}
-                </span>
-                <span className={`text-sm text-${item.pcColor} ml-2`}
-                >
+              <p className="mt-3">
+                <span className="text-lg font-semibold">{item.amount}</span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
                   {item.percentage}
                 </span>
+              </p> 
+              <p className="text-sm text-gray-400 mt-1">
+              {item.title}
+
+              </p>
+              
+            </div>
+          ))}
+
+        </div>
+      </div>
+
+      <div className='flex gap-10 flex-wrap justify-center'>
+        <div className='bg-white dark:text-gray-200 dark:bg-seconday-dark-bg m-3
+        p-4 rounded-2xl md:w-780'>
+          <div className='flex justify-between'>
+            <p className='font-semibold text-xl'> Estado de la batería</p>
+            <div className='flex items-center gap-4'>
+              <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'>
+                <span><GoPrimitiveDot/></span>
+                <span>Bateria Baja</span>
+              </p>
+              <p className='flex items-center gap-2 text-green-600 hover:drop-shadow-xl'>
+                <span><GoPrimitiveDot/></span>
+                <span>Bateria Alta</span>
               </p>
 
             </div>
-          ))}
+
+          </div>
+          <div className='mt-10 flex gap-10 flex-wrap justify-center'>
+            <div className='border-r-1 border-color m-4 pr-10'>
+              <div>
+                <p>
+                  <span className='text-3xl font-semibold'>130BPM</span>
+                  <span className='p-1.25 hover:drop-shadow-xl
+                  cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>110%
+                  </span>
+                </p>
+                <p className='text-gray-500 mt-1'>
+                  Budged</p>
+              </div>
+              <div className='mt-8'>
+                <p>
+                  <span className='text-3xl font-semibold'>80BPM</span>                </p>
+                <p className='text-gray-500 mt-1'>
+                  Expense</p>
+              </div>
+              <div className='m4-5'>
+                <SparkLine
+                  currentColor ="blue"
+                  id ="line-sparkline"
+                  type ="Line"
+                  height="80px"
+                  width ="250px"
+                  data ={SparklineAreaData}
+                  color = "blue"
+                  />
+
+              </div>
+
+            </div>
+
+          </div>
 
         </div>
 
